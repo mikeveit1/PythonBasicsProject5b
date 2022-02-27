b27290ba962b9fdeee8b3330a8df9cb18b22b9af
Michael Veit <mikeveit1@gmail.com>
2022-02-27 15:26:58 -0500

    changed camel casing to snake casing

--- taxicab.py
+++ taxicab.py
@@ -5,31 +5,31 @@ class Taxicab:
         not the Euclidean distance from its starting point). Has two methods that move the Taxicab object: one to move
         left/right(x), one to move up/down(y)"""
 
-    def __init__(self, xCoord, yCoord):
+    def __init__(self, x_coord, y_coord):
         """Initializes the Taxicab object with an x-coordinate parameter, a y-coordinate parameter,
             and the odometer with 0"""
-        self._xCoord = xCoord
-        self._yCoord = yCoord
+        self._x_coord = x_coord
+        self._y_coord = y_coord
         self._odometer = 0
 
     def get_x_coord(self):
         """Returns the current x coordinate"""
-        return self._xCoord
+        return self._x_coord
 
     def get_y_coord(self):
         """Returns the current y coordinate"""
-        return self._yCoord
+        return self._y_coord
 
     def get_odometer(self):
         """Returns the current odometer reading"""
         return self._odometer
 
-    def move_x(self, unitsToMove):
+    def move_x(self, units_to_move):
         """Takes in a parameter of units to move the Taxicab object left or right"""
-        self._xCoord += unitsToMove
-        self._odometer += abs(unitsToMove)
+        self._x_Coord += units_to_move
+        self._odometer += abs(units_to_move)
 
-    def move_y(self, unitsToMove):
+    def move_y(self, units_to_move):
         """Takes in a parameter of units to move the Taxicab object up or down"""
-        self._yCoord += unitsToMove
-        self._odometer += abs(unitsToMove)
+        self._y_coord += units_to_move
+        self._odometer += abs(units_to_move)
